b5bffc329450d12fc170633d8937850b997a5d25
jakub.durys <duryskuba@wp.pl>
2022-10-01 10:15:28 +0200

    init - accessmanagement project

---
+++ src/main/java/com/durys/jakub/accessmanagement/AccessmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.accessmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durys/jakub/accessmanagement/AccessmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.accessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
