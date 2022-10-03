568d60035fac2eb359f7e00ada9d7c27d50aed46
jakub.durys <duryskuba@wp.pl>
2022-10-03 18:54:49 +0200

    role entity

---
+++ src/main/java/com/durys/jakub/accessmanagement/role/model/Role.java
@@ -0,0 +1,26 @@
+package com.durys.jakub.accessmanagement.role.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "AM_ROLE")
+@Entity
+public class Role {
+
+    @Id
+    private String name;
+    private String desc;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+}
