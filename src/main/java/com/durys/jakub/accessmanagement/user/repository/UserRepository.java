9040ab3de39c3398249145dffbefedccb3c4c29d
jakub.durys <duryskuba@wp.pl>
2022-10-06 18:43:21 +0200

    user service

---
+++ src/main/java/com/durys/jakub/accessmanagement/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.accessmanagement.user.repository;
+
+import com.durys.jakub.accessmanagement.user.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/durys/jakub/accessmanagement/user/service/UserService.java
@@ -0,0 +1,23 @@
+package com.durys.jakub.accessmanagement.user.service;
+
+import com.durys.jakub.accessmanagement.user.model.entity.User;
+import com.durys.jakub.accessmanagement.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(RuntimeException::new); //todo
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(RuntimeException::new); //todo
+    }
+}
