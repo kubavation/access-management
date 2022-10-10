a0955c3c11174087a0c96a46f59e8dc1cddd648e
jakub.durys <duryskuba@wp.pl>
2022-10-10 18:57:14 +0200

    addition config

--- src/main/java/com/durys/jakub/accessmanagement/security/config/SecurityConfiguration.java
+++ src/main/java/com/durys/jakub/accessmanagement/security/config/SecurityConfiguration.java
@@ -1,20 +1,30 @@
 package com.durys.jakub.accessmanagement.security.config;
 
+import com.durys.jakub.accessmanagement.user.service.AmUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true,
+    securedEnabled = true
+)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final int PASSWORD_STRENGTH = 5;
+    private final AmUserDetailsService amUserDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
+        auth.userDetailsService(amUserDetailsService);
     }
 
     @Bean
