887b2c41acadb616007c702bfbb60d0b1cfed510
jakub.durys <duryskuba@wp.pl>
2022-10-10 19:42:14 +0200

    create token method

--- src/main/java/com/durys/jakub/accessmanagement/security/util/JwtSecurityUtils.java
+++ src/main/java/com/durys/jakub/accessmanagement/security/util/JwtSecurityUtils.java
@@ -1,16 +1,25 @@
 package com.durys.jakub.accessmanagement.security.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import liquibase.pro.packaged.D;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Map;
 
 @Service
 public class JwtSecurityUtils {
 
+    private static final String SECRET_KEY = "am-secret-key";
 
 
     private String createToken(String subject, Map<String, Object> claims) {
-
-        return null;
+        return Jwts.builder()
+                .addClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date()) //todo
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 }
