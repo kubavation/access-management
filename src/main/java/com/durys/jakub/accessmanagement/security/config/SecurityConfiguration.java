17a6f017283ce34f80df9fc1920ce09ed00064e6
jakub.durys <duryskuba@wp.pl>
2022-10-10 19:12:46 +0200

    more security config

--- src/main/java/com/durys/jakub/accessmanagement/security/config/SecurityConfiguration.java
+++ src/main/java/com/durys/jakub/accessmanagement/security/config/SecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.durys.jakub.accessmanagement.security.config;
 
+import com.durys.jakub.accessmanagement.security.filter.JwtFilter;
 import com.durys.jakub.accessmanagement.user.service.AmUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -21,6 +25,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final int PASSWORD_STRENGTH = 5;
     private final AmUserDetailsService amUserDetailsService;
 
+    private final JwtFilter jwtFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
