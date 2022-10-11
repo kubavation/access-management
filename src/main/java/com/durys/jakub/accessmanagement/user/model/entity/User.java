97a6bb76a4e9de06ff42ea8a8075f98aaedc2850
jakub.durys <duryskuba@wp.pl>
2022-10-11 17:36:13 +0200

    fix in am_user liquibase

--- src/main/java/com/durys/jakub/accessmanagement/user/model/entity/User.java
+++ src/main/java/com/durys/jakub/accessmanagement/user/model/entity/User.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -19,6 +16,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
