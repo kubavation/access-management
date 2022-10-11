e8ab10686b1b11febf8a8ff0835fa78d1472758b
jakub.durys <duryskuba@wp.pl>
2022-10-11 16:13:00 +0200

    basic structure + models of authentication

---
+++ src/main/java/com/durys/jakub/accessmanagement/authentication/controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.durys.jakub.accessmanagement.authentication.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+}
---
+++ src/main/java/com/durys/jakub/accessmanagement/authentication/model/AuthenticationResponse.java
@@ -0,0 +1,8 @@
+package com.durys.jakub.accessmanagement.authentication.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
+}
