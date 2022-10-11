443cb214a305d9e3799e5afc1968769397b762a8
jakub.durys <duryskuba@wp.pl>
2022-10-11 16:39:01 +0200

    authentication service

--- src/main/java/com/durys/jakub/accessmanagement/authentication/controller/AuthenticationController.java
+++ src/main/java/com/durys/jakub/accessmanagement/authentication/controller/AuthenticationController.java
@@ -1,9 +1,23 @@
 package com.durys.jakub.accessmanagement.authentication.controller;
 
+import com.durys.jakub.accessmanagement.authentication.model.AuthenticationRequest;
+import com.durys.jakub.accessmanagement.authentication.model.AuthenticationResponse;
+import com.durys.jakub.accessmanagement.authentication.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/auth")
+
+@RestController("/authentication")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
---
+++ src/main/java/com/durys/jakub/accessmanagement/authentication/service/AuthenticationService.java
@@ -0,0 +1,46 @@
+package com.durys.jakub.accessmanagement.authentication.service;
+
+import com.durys.jakub.accessmanagement.authentication.model.AuthenticationRequest;
+import com.durys.jakub.accessmanagement.authentication.model.AuthenticationResponse;
+import com.durys.jakub.accessmanagement.security.util.JwtSecurityUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtSecurityUtils jwtSecurityUtils;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest auth) {
+
+        log.info("authentication - start with username {}", auth.getUsername());
+
+        try {
+
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(auth.getUsername(), auth.getPassword())
+            );
+
+        } catch (AuthenticationException ex) {
+            log.error("error in authentication", ex);
+            throw new RuntimeException("TODO");
+        }
+
+        UserDetails user = userDetailsService.loadUserByUsername(auth.getUsername());
+        String token = jwtSecurityUtils.generateToken(user.getUsername());
+
+        log.info("authentication - successful with user {}", auth.getUsername());
+        
+        return new AuthenticationResponse(token);
+    }
+}
