182d46f65a7081fcdb5380a033ab05439216b8bb
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-11 14:39:53 +0100

    fix in swagger enpoint - security

--- src/main/java/com/durys/jakub/accessmanagement/security/config/KeycloakSecurityConfig.java
+++ src/main/java/com/durys/jakub/accessmanagement/security/config/KeycloakSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -14,24 +15,29 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @KeycloakConfiguration
-public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter { //2
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception { //3
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
-                .authorizeRequests()
-                .anyRequest().permitAll();
+            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
+            .authorizeRequests()
+            .anyRequest().authenticated();
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) { //4
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(getKeycloakAuthenticationProvider());
     }
 
-    private KeycloakAuthenticationProvider getKeycloakAuthenticationProvider() { //5
+    private KeycloakAuthenticationProvider getKeycloakAuthenticationProvider() {
         KeycloakAuthenticationProvider authenticationProvider = keycloakAuthenticationProvider();
         var mapper = new SimpleAuthorityMapper();
         mapper.setConvertToUpperCase(true);
@@ -42,7 +48,7 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
 
     @Bean
     @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() { //6
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 }
