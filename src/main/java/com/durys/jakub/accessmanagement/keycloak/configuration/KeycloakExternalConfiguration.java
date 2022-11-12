1e284bd65dcab1ef61cf77b4287b277f5e2149c3
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-12 17:36:34 +0100

    keycloak client configuration

---
+++ src/main/java/com/durys/jakub/accessmanagement/keycloak/configuration/KeycloakExternalConfiguration.java
@@ -0,0 +1,24 @@
+package com.durys.jakub.accessmanagement.keycloak.configuration;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+class KeycloakExternalConfiguration {
+
+    @Value("${keycloak.auth-server-url}")
+    private String keycloakServer;
+
+    @Value("${keycloak.realm}")
+    private String keycloakRealm;
+
+
+    @Bean
+    public WebClient.Builder keycloakClient() {
+       return WebClient.builder()
+               .baseUrl(String.format("%s/admin/realms/%s", keycloakServer, keycloakRealm));
+    }
+}
