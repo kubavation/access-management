69a0ecf87985e746772275d7a5b6fc56eb70d554
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-12 21:55:16 +0100

    cleaning + new config

--- src/main/java/com/durys/jakub/accessmanagement/keycloak/configuration/KeycloakExternalConfiguration.java
+++ src/main/java/com/durys/jakub/accessmanagement/keycloak/configuration/KeycloakExternalConfiguration.java
@@ -1,6 +1,10 @@
 package com.durys.jakub.accessmanagement.keycloak.configuration;
 
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +19,23 @@ class KeycloakExternalConfiguration {
     @Value("${keycloak.realm}")
     private String keycloakRealm;
 
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    @Value("${keycloak.credentials.secret}")
+    private String secret;
+
+
 
     @Bean(name = "keycloakClient")
-    public WebClient keycloakClient() {
-       return WebClient.builder()
-               .baseUrl(String.format("%s/admin/realms/%s", keycloakServer, keycloakRealm))
-               .build();
+    Keycloak keycloakClient() {
+        return KeycloakBuilder.builder()
+                .serverUrl(keycloakServer)
+                .grantType(OAuth2Constants.PASSWORD)
+                .realm(keycloakRealm).clientId(clientId)
+                .username("admin").password("admin")
+                .clientSecret(secret)
+                .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
+                .build();
     }
 }
