c811515abdb6396ff8054fad5977681f14d8acdd
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-13 16:51:13 +0100

    delete unused classes

--- src/main/java/com/durys/jakub/accessmanagement/role/mappers/RoleMapper.java
+++ src/main/java/com/durys/jakub/accessmanagement/role/mappers/RoleMapper.java
@@ -1,8 +1,6 @@
 package com.durys.jakub.accessmanagement.role.mappers;
 
 import com.durys.jakub.accessmanagement.role.model.dto.RoleDTO;
-import com.durys.jakub.accessmanagement.role.model.entity.Role;
-import com.durys.jakub.accessmanagement.role.repository.RoleRepository;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.keycloak.representations.idm.RoleRepresentation;
@@ -11,14 +9,6 @@ import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RoleMapper {
-
-    public static Role toEntity(RoleDTO dto) {
-        return new Role(dto.getName(), dto.getDescription());
-    }
-
-    public static RoleDTO toDTO(Role entity) {
-        return new RoleDTO(entity.getName(), entity.getDesc());
-    }
 
     public static RoleDTO toDTO(RoleRepresentation role) {
         return new RoleDTO(role.getName(), role.getDescription());
