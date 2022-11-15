a1d24f07ecad18fc957a9fc90a9339cb22497d9a
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-15 20:53:47 +0100

    generating temp password

--- src/main/java/com/durys/jakub/accessmanagement/keycloak/KeycloakPasswordGenerator.java
+++ src/main/java/com/durys/jakub/accessmanagement/keycloak/KeycloakPasswordGenerator.java
@@ -2,13 +2,24 @@ package com.durys.jakub.accessmanagement.keycloak;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.keycloak.representations.idm.CredentialRepresentation;
 
 import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 class KeycloakPasswordGenerator {
 
-    static String generate() {
+    private final static String PASSWORD_TYPE = "password";
+
+    private static String generatePassword() {
         return UUID.randomUUID().toString();
+    }
+
+    static CredentialRepresentation credentialRepresentation() {
+        CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+        credentialRepresentation.setTemporary(true);
+        credentialRepresentation.setType(PASSWORD_TYPE);
+        credentialRepresentation.setValue(generatePassword());
+        return credentialRepresentation;
     }
 }
