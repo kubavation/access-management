5b07e75c30d48dcb5eec497218fe479a0a958dde
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-16 18:15:10 +0100

    mailDto static builder

--- src/main/java/com/durys/jakub/accessmanagement/user/mail_client/model/MailWithTemporaryPasswordDTO.java
+++ src/main/java/com/durys/jakub/accessmanagement/user/mail_client/model/MailWithTemporaryPasswordDTO.java
@@ -1,10 +1,16 @@
 package com.durys.jakub.accessmanagement.user.mail_client.model;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MailWithTemporaryPasswordDTO {
     private String destination;
     private String title;
     private String content;
+
+    public static MailWithTemporaryPasswordDTO from(String destination, String password) {
+        return new MailWithTemporaryPasswordDTO(destination, "Account successfully created", String.format("Your temporary password: %s", password));
+    }
 }
