a734a62370b14cda202fad5edfccc2a331db4a46
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-26 17:58:06 +0200

    changes in User model

--- src/main/java/com/durys/jakub/accessmanagement/user/model/User.java
+++ src/main/java/com/durys/jakub/accessmanagement/user/model/User.java
@@ -1,15 +1,28 @@
 package com.durys.jakub.accessmanagement.user.model;
 
-import lombok.AllArgsConstructor;
+import com.durys.jakub.accessmanagement.role.model.Role;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.Collections;
+import java.util.Set;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     private String id;
     private String username;
     private String email;
     private boolean enabled;
+    private Set<Role> roles;
+
+    public User(String id, String username, String email, boolean enabled, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
+
+    public User(String id, String username, String email, boolean enabled) {
+        this(id, username, email, enabled, Collections.emptySet());
+    }
 }
