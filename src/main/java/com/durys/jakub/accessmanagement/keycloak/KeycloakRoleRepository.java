bd82b8e90c71896d31e852e40419669e7adeafb5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-27 18:27:00 +0200

    implementation of KeycloakRoleRepository

--- src/main/java/com/durys/jakub/accessmanagement/keycloak/KeycloakRoleRepository.java
+++ src/main/java/com/durys/jakub/accessmanagement/keycloak/KeycloakRoleRepository.java
@@ -3,6 +3,7 @@ package com.durys.jakub.accessmanagement.keycloak;
 import com.durys.jakub.accessmanagement.role.RoleRepository;
 import com.durys.jakub.accessmanagement.role.model.Role;
 import lombok.RequiredArgsConstructor;
+import org.keycloak.representations.idm.RoleRepresentation;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,21 +15,25 @@ public class KeycloakRoleRepository implements RoleRepository {
 
     @Override
     public List<Role> roles() {
-        return null;
+        return KeycloakRoleConverter.instance().toRoles(keycloakClientService.roles());
     }
 
     @Override
     public Optional<Role> findById(String id) {
-        return Optional.empty();
+        return roles().stream()
+                .filter(r -> r.name().equals(id))
+                .findFirst();
     }
 
     @Override
     public void save(Role role) {
-
+        RoleRepresentation representation = KeycloakRoleConverter.instance().toRepresentation(role);
+        keycloakClientService.roles().add(representation);
     }
 
     @Override
     public void delete(Role role) {
-
+        RoleRepresentation representation = KeycloakRoleConverter.instance().toRepresentation(role);
+        keycloakClientService.roles().remove(representation);
     }
 }
