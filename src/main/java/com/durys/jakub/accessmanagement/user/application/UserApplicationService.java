faa9d8cd01c064cad4b4d34c214e83bec3677cde
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-09 19:23:05 +0200

    usage of userValidator in application service

--- src/main/java/com/durys/jakub/accessmanagement/user/application/UserApplicationService.java
+++ src/main/java/com/durys/jakub/accessmanagement/user/application/UserApplicationService.java
@@ -3,6 +3,8 @@ package com.durys.jakub.accessmanagement.user.application;
 import com.durys.jakub.accessmanagement.ddd.annotation.ApplicationService;
 import com.durys.jakub.accessmanagement.user.domain.User;
 import com.durys.jakub.accessmanagement.user.domain.UserRepository;
+import com.durys.jakub.accessmanagement.user.domain.UserValidator;
+import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
@@ -12,9 +14,19 @@ import java.util.UUID;
 public class UserApplicationService {
 
     private final UserRepository userRepository;
-
+    
     public void create(String username, String email) {
+
         User user = new User(UUID.randomUUID().toString(), username, email);
+
+        Either<UserValidator.ValidationError, Void> validationResult = UserValidator
+                .instance(userRepository)
+                .validateUser(user);
+
+        if (validationResult.isLeft()) {
+            //todo handle validationResult.getLeft()
+        }
+
         userRepository.save(user);
     }
 
