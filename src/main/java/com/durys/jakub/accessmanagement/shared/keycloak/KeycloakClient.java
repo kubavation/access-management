352617c15864172c3bcb678ed1126561d7885e95
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-16 20:52:23 +0200

    removed unused methods in keycloak client

--- src/main/java/com/durys/jakub/accessmanagement/shared/keycloak/KeycloakClient.java
+++ src/main/java/com/durys/jakub/accessmanagement/shared/keycloak/KeycloakClient.java
@@ -5,10 +5,8 @@ import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.RolesResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.RoleRepresentation;
-import org.keycloak.representations.idm.UserRepresentation;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -22,27 +20,6 @@ public class KeycloakClient {
 
     public RolesResource rolesResource() {
         return realmResource.roles();
-    }
-
-    public List<UserRepresentation> users() {
-        return realmResource.users().list();
-    }
-
-    public Optional<UserRepresentation> userById(String id) {
-        return users()
-                .stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
-    }
-
-    public List<RoleRepresentation> roles() {
-        return realmResource.roles().list();
-    }
-
-    public Optional<RoleRepresentation> roleById(String id) {
-        return roles().stream()
-                .filter(role -> role.getName().equals(id))
-                .findFirst();
     }
 
 
